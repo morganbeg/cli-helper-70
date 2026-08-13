c1641c271787d051bcde96fa3ed5785f8a32f71b
Elena Dimitrova <morganbelanger917@gmail.com>
2026-08-13 17:15:10 +0300

    refactor:logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,11 +1,10 @@
 import logging
-import os
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(name, log_file, level=logging.INFO):
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=3)
+def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
+    logger = logging.getLogger('game_logger')
+    logger.setLevel(logging.DEBUG)
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
@@ -13,7 +12,5 @@ def setup_logger(name, log_file, level=logging.INFO):
 
 # Example usage
 if __name__ == '__main__':
-    log = setup_logger('my_logger', 'app.log')
-    log.info('This is an info message')
-    log.error('This is an error message')
-    log.debug('This is a debug message')
+    log = setup_logger()
+    log.info('Logger is set up and ready.')
